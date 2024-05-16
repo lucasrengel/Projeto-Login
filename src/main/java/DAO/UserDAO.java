b3b2f241ea3cf1f32814bda9214cf3cd178187f7
lucasrengel <lucas_rengel@estudante.sc.senai.br>
2024-05-15 21:04:33 -0300

    Classe pro User modificada e Classe para conexao/interacao com o banco de dados UserDAO criada

--- src/main/java/DAO/UserDAO.java
+++ src/main/java/DAO/UserDAO.java
@@ -1,28 +1,137 @@
 package DAO;
 
 import Model.User;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class UserDAO {
 
-    public void insertUser(User user){
+    public static ArrayList<User> myList = new ArrayList<>();
 
-        String sql = "INSERT INTO users (name, password) VALUES (?, ?)";
+    public java.sql.Connection getConecction() {
 
-        PreparedStatement ps = null;
+        java.sql.Connection connection = null;
 
         try {
-            ps = Connection.getConecction().prepareStatement(sql);
-            ps.setString(1, user.getName());
-            ps.setString(2, user.getPassword());
 
-            ps.execute();
-            ps.close();
+            String driver = "com.mysql.cj.jdbc.Driver";
+            Class.forName(driver);
+
+            String server = "localhost";
+            String database = "projetologin";
+            String url = "jdbc:mysql://" + server + ":3306/" + database;
+            String user = "root";
+            String password = "1234";
+
+            connection = DriverManager.getConnection(url, user, password);
+
+            // Testando..
+            if (connection != null) {
+                System.out.println("Status: Conectado!");
+            } else {
+                System.out.println("Status: NAO CONECTADO!");
+            }
+
+            return connection;
+
+        } catch (ClassNotFoundException e) {  //Driver nao encontrado
+            System.out.println("O driver nao foi encontrado. " + e.getMessage());
+            return null;
 
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            System.out.println("Nao foi possivel conectar...");
+            return null;
+        }
+    }
+
+    public ArrayList getMyList() {
+
+        myList.clear();
+
+        try {
+            Statement stmt = this.getConecction().createStatement();
+            ResultSet res = stmt.executeQuery("SELECT * FROM tb_users");
+            while (res.next()) {
+
+                int id = res.getInt("id");
+                String name = res.getString("name");
+                String password = res.getString("password");
+
+                User object = new User(id, name, password);
+
+                myList.add(object);
+            }
+
+            stmt.close();
+
+        } catch (SQLException ex) {
+        }
+
+        return myList;
+    }
+
+    public int higherID() throws SQLException {
+
+        int maiorID = 0;
+        try {
+            Statement stmt = this.getConecction().createStatement();
+            ResultSet res = stmt.executeQuery("SELECT MAX(id) id FROM tb_users");
+            res.next();
+            maiorID = res.getInt("id");
+
+            stmt.close();
+
+        } catch (SQLException ex) {
+        }
+
+        return maiorID;
+    }
+
+    public boolean InsertUserBD(User object) {
+        String sql = "INSERT INTO tb_users(id,name,password) VALUES(?,?,?)";
+
+        try {
+            PreparedStatement stmt = this.getConecction().prepareStatement(sql);
+
+            stmt.setInt(1, object.getId());
+            stmt.setString(2, object.getName());
+            stmt.setString(3, object.getPassword());
+
+            stmt.execute();
+            stmt.close();
+
+            return true;
+
+        } catch (SQLException erro) {
+            throw new RuntimeException(erro);
+        }
+    }
+
+    public boolean SelectUserBD(String name, String password) {
+        String sql = "SELECT * FROM tb_users WHERE name = ? AND password = ?";
+
+        try {
+            Connection connection = getConecction();
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, name);
+            stmt.setString(2, password);
+
+            ResultSet res = stmt.executeQuery();
+            boolean userExists = res.next(); 
+
+            stmt.close();
+            connection.close(); 
+
+            return userExists;
+
+        } catch (SQLException erro) {
+            throw new RuntimeException(erro);
         }
     }
 }
--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -1,17 +1,54 @@
 package Model;
 
+import DAO.UserDAO;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class User {
 
-    private int iduser;
+    public static ArrayList<User> myList;
+
+    private int id;
     private String name;
     private String password;
+    private final UserDAO dao;
 
-    public int getIduser() {
-        return iduser;
+    public User() {
+        this.dao = new UserDAO();
     }
 
-    public void setIduser(int iduser) {
-        this.iduser = iduser;
+    public User(int id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.dao = new UserDAO();
+    }
+
+    public ArrayList getMyList() {
+        return dao.getMyList();
+    }
+
+    public boolean insertUserBD(String name, String password) throws SQLException {
+        int id = this.higherID() + 1;
+        User object = new User(id, name, password);
+        dao.InsertUserBD(object);
+        return true;
+    }
+
+    public boolean selectUserBD(String name, String password) throws SQLException{
+        return dao.SelectUserBD(name, password);
+    }
+
+    public int higherID() throws SQLException {
+        return dao.higherID();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -28,5 +65,11 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + id + ", name="
+                + name;
     }
 }

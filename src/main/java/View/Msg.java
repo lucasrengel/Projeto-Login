bb5c4a0c695124d7f816caadabbb1d702d39e916
lucasrengel <lucas_rengel@estudante.sc.senai.br>
2024-05-15 21:06:01 -0300

    Tela login e register criadas para expandir a funcionalidade do aplicativo. Classe Msg criada.

---
+++ src/main/java/View/Msg.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package View;
+
+/**
+ *
+ * @author lucas
+ */
+public class Msg extends Exception{
+    Msg(String msg){
+        super(msg);
+    }
+}
